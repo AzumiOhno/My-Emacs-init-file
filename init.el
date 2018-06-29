b1b95c0f8ed297cb45275eff3cd0c433d097e246
AzumiOhno <ohno.mukailab@gmail.com>
2018-06-29 19:55:57 +0900

    add yasnippets

--- init.el
+++ init.el
@@ -430,3 +430,20 @@
 (add-to-list 'exec-path "~/.pyenv/shims")
 (setq elpy-rpc-backend "jedi")
 (add-to-list 'load-path "~/.pyenv/versions/bin/")
+
+;;; yasnipets
+;; 自分用・追加用テンプレート -> mysnippetに作成したテンプレートが格納される
+(require 'yasnippet)
+(setq yas-snippet-dirs
+      '("~/.emacs.d/mysnippets"
+        "~/.emacs.d/yasnippets"
+        ))
+
+;; 既存スニペットを挿入する
+(define-key yas-minor-mode-map (kbd "C-x i i") 'yas-insert-snippet)
+;; 新規スニペットを作成するバッファを用意する
+(define-key yas-minor-mode-map (kbd "C-x i n") 'yas-new-snippet)
+;; 既存スニペットを閲覧・編集する
+(define-key yas-minor-mode-map (kbd "C-x i v") 'yas-visit-snippet-file)
+
+(yas-global-mode 1)
