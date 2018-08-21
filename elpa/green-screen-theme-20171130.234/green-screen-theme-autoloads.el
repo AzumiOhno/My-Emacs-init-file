866b6c1ce6d3993a3f9f5fbc0b3fb8d1eee5637b
AzumiOhno <ohno.mukailab@gmail.com>
2018-08-21 19:00:03 +0900

    delete extra files

--- elpa/green-screen-theme-20171130.234/green-screen-theme-autoloads.el
+++
@@ -1,16 +0,0 @@
-;;; green-screen-theme-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil nil ("green-screen-theme.el") (23344 50860
-;;;;;;  369827 527000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; green-screen-theme-autoloads.el ends here
