d32e87add82f7ab4b3f08eac47bbe830bd4e6171
AzumiOhno <ohno.mukailab@gmail.com>
2018-08-21 19:00:03 +0900

    delete extra files

--- elpa/dired-toggle-sudo-20151109.206/dired-toggle-sudo-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "dired-toggle-sudo" "20151109.206" "Browse directory with sudo privileges." 'nil :commit "02449dbda4e168f99fe5352c9628df5d39e11483" :keywords '("emacs" "dired"))
--- elpa/treemacs-projectile-20180614.1021/treemacs-projectile-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "treemacs-projectile" "20180614.1021" "Projectile integration for treemacs" '((projectile "0.14.0") (treemacs "0")) :commit "ac4d8fcb1a40c3bec18ac888b5dd56e0a4836bc7" :url "https://github.com/Alexander-Miller/treemacs")
